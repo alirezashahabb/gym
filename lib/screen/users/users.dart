3cc3ec9c9b78f0091904ca4f9dd0af0f41de5467
alireza shahabadini <shehabalireza@gmail.com>
2024-12-18 10:14:28 +0330

    add listView to user Screen

--- lib/screen/users/users.dart
+++ lib/screen/users/users.dart
@@ -14,19 +14,22 @@ class UsersScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: themeData.colorScheme.primary,
-        onPressed: () {},
-        child: const Icon(
-          Icons.add,
-          color: CustomColors.kWhiteColor,
-        ),
-      ),
+      floatingActionButton: isActive
+          ? FloatingActionButton(
+              backgroundColor: themeData.colorScheme.primary,
+              onPressed: () {},
+              child: const Icon(
+                Icons.add,
+                color: CustomColors.kWhiteColor,
+              ),
+            )
+          : null,
       body: SafeArea(
           child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
         child: Column(
           children: [
+            // for light and dark them so locale
             Row(
               children: [
                 Expanded(
@@ -72,7 +75,90 @@ class UsersScreen extends StatelessWidget {
                   ),
                 ),
               ],
-            )
+            ),
+
+            // for search text field
+            TextField(
+              decoration: InputDecoration(
+                fillColor: themeData.colorScheme.tertiary,
+                filled: true,
+                hintText: LocaleKeys.search_by_fullName.tr(),
+                hintStyle: themeData.textTheme.bodyMedium,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                contentPadding: const EdgeInsets.all(12),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            // for Active and DeActive Users
+            Container(
+              padding: const EdgeInsets.all(8),
+              height: 40,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: isActive
+                    ? CustomColors.kGreenColor
+                    : CustomColors.kRedColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    isActive
+                        ? LocaleKeys.count_of_active_user.tr()
+                        : LocaleKeys.count_of_inactive_user.tr(),
+                    style: themeData.textTheme.bodyMedium!.copyWith(
+                      color: CustomColors.kWhiteColor,
+                    ),
+                  ),
+                  Text(
+                    LocaleKeys.people.tr(
+                      namedArgs: {"count": "14"},
+                    ),
+                    style: themeData.textTheme.bodyMedium!.copyWith(
+                      color: CustomColors.kWhiteColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            //for ListView Builder
+            Expanded(
+                child: ListView.builder(
+              itemCount: 3,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  decoration: BoxDecoration(
+                      color: themeData.colorScheme.tertiary,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    children: [
+                      const Expanded(child: Text('alireza')),
+                      const Expanded(child: Text('1403/12/5')),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(FluentIcons.edit_24_regular),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(FluentIcons.delete_12_filled),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )),
           ],
         ),
       )),
