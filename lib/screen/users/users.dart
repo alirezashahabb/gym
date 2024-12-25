178b51359d4b2096217ebf4d0428f4bebf00b01b
alireza shahabadini <shehabalireza@gmail.com>
2024-12-25 11:14:59 +0330

    complement  to local host DataBase

--- lib/screen/users/users.dart
+++ lib/screen/users/users.dart
@@ -9,17 +9,42 @@ import 'package:flutter_application_2/model/user_model.dart';
 import 'package:flutter_application_2/screen/add_edite/add_edite.dart';
 import 'package:flutter_application_2/translations/locale_keys.g.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:persian_datetime_picker/persian_datetime_picker.dart';
 import 'package:quickalert/models/quickalert_type.dart';
 import 'package:quickalert/widgets/quickalert_dialog.dart';
 
 class UsersScreen extends StatelessWidget {
   final bool isActive;
+
   const UsersScreen({super.key, this.isActive = true});
 
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
     var box = Hive.box<User>(AppConstant.userBox);
+    ValueNotifier<String> searchNotifier = ValueNotifier('');
+// this function for chaeck users is Active or no
+    List<User> getUser() {
+      List<User> userList = [];
+      for (var element in box.values) {
+        var jalaliDate1 = Jalali.now().toDateTime();
+        int endDateYear = int.parse(element.endDate!.split('/')[0]);
+        int endDateMonth = int.parse(element.endDate!.split('/')[1]);
+        int endDateDay = int.parse(element.endDate!.split('/')[2]);
+        var jaladate2 =
+            Jalali(endDateYear, endDateMonth, endDateDay).toDateTime();
+        if (isActive) {
+          if (jaladate2.isAfter(jalaliDate1)) {
+            userList.add(element);
+          }
+        } else {
+          if (!jaladate2.isAfter(jalaliDate1)) {
+            userList.add(element);
+          }
+        }
+      }
+      return userList;
+    }
 
     return Scaffold(
       floatingActionButton: isActive
@@ -102,6 +127,9 @@ class UsersScreen extends StatelessWidget {
 
             // for search text field
             TextField(
+              onChanged: (value) {
+                searchNotifier.value = value;
+              },
               decoration: InputDecoration(
                 fillColor: themeData.colorScheme.tertiary,
                 filled: true,
@@ -144,7 +172,9 @@ class UsersScreen extends StatelessWidget {
                       ),
                       Text(
                         LocaleKeys.people.tr(
-                          namedArgs: {"count": "${value.length}"},
+                          namedArgs: {
+                            "count": getUser().length.toString(),
+                          },
                         ),
                         style: themeData.textTheme.bodyMedium!.copyWith(
                           color: CustomColors.kWhiteColor,
@@ -160,73 +190,92 @@ class UsersScreen extends StatelessWidget {
             ),
             //for ListView Builder
             Expanded(
-              child: ValueListenableBuilder(
-                valueListenable: box.listenable(),
-                builder: (context, value, child) {
-                  return value.isNotEmpty
-                      ? ListView.builder(
-                          itemCount: box.values.length,
-                          itemBuilder: (context, index) {
-                            var items = box.values.toList()[index];
-                            return Container(
-                              margin: const EdgeInsets.symmetric(vertical: 10),
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 8, horizontal: 8),
-                              decoration: BoxDecoration(
-                                  color: themeData.colorScheme.tertiary,
-                                  borderRadius: BorderRadius.circular(8)),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(items.fullName!),
-                                  ),
-                                  Expanded(
-                                    child: Text(items.registerData!),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      Navigator.of(context)
-                                          .push(MaterialPageRoute(
-                                        builder: (context) =>
-                                            AddEditScreen(user: items),
-                                      ));
-                                    },
-                                    icon:
-                                        const Icon(FluentIcons.edit_24_regular),
-                                  ),
-                                  IconButton(
-                                    onPressed: () {
-                                      QuickAlert.show(
-                                        context: context,
-                                        type: QuickAlertType.confirm,
-                                        title: LocaleKeys.conform_delete.tr(),
-                                        confirmBtnText: LocaleKeys.yse.tr(),
-                                        cancelBtnText: LocaleKeys.no.tr(),
-                                        confirmBtnColor: Colors.green,
-                                        onConfirmBtnTap: () async {
-                                          await items.delete();
-                                          Navigator.pop(context);
-                                        },
-                                        onCancelBtnTap: () {
-                                          Navigator.pop(context);
-                                        },
-                                      );
-                                    },
-                                    icon: const Icon(
-                                        FluentIcons.delete_12_filled),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        )
-                      : Assets.img.emptyPana.image(
-                          width: 200,
-                          height: 200,
-                        );
-                },
-              ),
-            ),
+                child: ValueListenableBuilder(
+              valueListenable: searchNotifier,
+              builder: (context, value, child) {
+                return ValueListenableBuilder(
+                  valueListenable: box.listenable(),
+                  builder: (context, value, child) {
+                    // for search by Name
+                    List<User> users = getUser();
+
+                    if (searchNotifier.value.isEmpty) {
+                      users = getUser();
+                    } else {
+                      users = users
+                          .where(
+                            (element) => element.fullName!
+                                .toLowerCase()
+                                .contains(searchNotifier.value.toLowerCase()),
+                          )
+                          .toList();
+                    }
+                    return users.isNotEmpty
+                        ? ListView.builder(
+                            itemCount: users.length,
+                            itemBuilder: (context, index) {
+                              var items = users[index];
+                              return Container(
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 8),
+                                decoration: BoxDecoration(
+                                    color: themeData.colorScheme.tertiary,
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: Text(items.fullName!),
+                                    ),
+                                    Expanded(
+                                      child: Text(items.registerData!),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .push(MaterialPageRoute(
+                                          builder: (context) =>
+                                              AddEditScreen(user: items),
+                                        ));
+                                      },
+                                      icon: const Icon(
+                                          FluentIcons.edit_24_regular),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {
+                                        QuickAlert.show(
+                                          context: context,
+                                          type: QuickAlertType.confirm,
+                                          title: LocaleKeys.conform_delete.tr(),
+                                          confirmBtnText: LocaleKeys.yse.tr(),
+                                          cancelBtnText: LocaleKeys.no.tr(),
+                                          confirmBtnColor: Colors.green,
+                                          onConfirmBtnTap: () async {
+                                            await items.delete();
+                                            Navigator.pop(context);
+                                          },
+                                          onCancelBtnTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                        );
+                                      },
+                                      icon: const Icon(
+                                          FluentIcons.delete_12_filled),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          )
+                        : Assets.img.emptyPana.image(
+                            width: 200,
+                            height: 200,
+                          );
+                  },
+                );
+              },
+            )),
           ],
         ),
       )),
